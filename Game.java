4314e2ef0ea4b184dd09b13cecf29807c9fb4648
FelpBSouza <fsouzab13@hotmail.com>
2022-09-08 12:18:25 -0300

    End of Java Pong Game

--- Game.java
+++ Game.java
@@ -17,36 +17,40 @@ public class Game extends Canvas implements Runnable, KeyListener{
             
             public BufferedImage layer = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
 
-            public Player player;
+            public static Player player;
+            public static Enemy enemy;
+            public static Ball ball;
         
 
 public Game(){
 
-        this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
-        this.addKeyListener(this);
-        player = new Player(100,HEIGHT-10);
-
+            this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
+            this.addKeyListener(this);
+            player = new Player(100,HEIGHT-10);
+            enemy = new Enemy(100,0);
+            ball = new Ball(100,HEIGHT/2 -1);
 
     }
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
-        Game game = new Game();
-        JFrame frame = new JFrame("Pong");
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(game);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-
-        new Thread(game).start();
+            Game game = new Game();
+            JFrame frame = new JFrame("Pong");
+            frame.setResizable(false);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(game);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            new Thread(game).start();
 
     }
 
     public void tick(){
 
-        player.tick();
+                player.tick();
+                enemy.tick();
+                ball.tick();
 
     }
 
@@ -54,34 +58,38 @@ public Game(){
 
         BufferStrategy bs = this.getBufferStrategy();
 
-        if( bs == null){
+            if( bs == null){
 
-            this.createBufferStrategy(3);
+                    this.createBufferStrategy(3);
             return;
         }
-        Graphics g = layer.getGraphics();
-        g.setColor(Color.black);
-        g.fillRect(0,0,WIDTH,HEIGHT);
-        player.render(g);
+            Graphics g = layer.getGraphics();
+            g.setColor(Color.black);
+            g.fillRect(0,0,WIDTH,HEIGHT);
+            player.render(g);
+            enemy.render(g);
+            ball.render(g);
+        
 
-
-        g = bs.getDrawGraphics();
-        g.drawImage(layer,0,0,WIDTH*SCALE,HEIGHT*SCALE,null);
-
-        bs.show();
+            g = bs.getDrawGraphics();
+            g.drawImage(layer,0,0,WIDTH*SCALE,HEIGHT*SCALE,null);
+            bs.show();
     }
 
     public void run(){
 
         while(true){
+
             tick();
             render();
 
             try{
+
                 Thread.sleep(1000/60);
 
             }
             catch(InterruptedException e){
+
                 e.printStackTrace();
             }
             
@@ -92,9 +100,11 @@ public Game(){
     public void keyPressed(KeyEvent e){
 
         if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+
             player.right = true;
         }
         else if (e.getKeyCode ()== KeyEvent.VK_LEFT){
+
             player.left = true;
         }
     }
@@ -113,9 +123,4 @@ public Game(){
     public void keyTyped(KeyEvent arg0){
 
     }
-
-
-
-
-
 }
